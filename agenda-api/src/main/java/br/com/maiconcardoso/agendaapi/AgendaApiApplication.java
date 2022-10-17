cf58502b72f6a0e1cc28770709ca794bb343530f
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-17 07:09:22 -0300

    Função para inserir entidade na tabela do banco de dados por meio do CommandLineRunner, foi deletado

--- agenda-api/src/main/java/br/com/maiconcardoso/agendaapi/AgendaApiApplication.java
+++ agenda-api/src/main/java/br/com/maiconcardoso/agendaapi/AgendaApiApplication.java
@@ -1,27 +1,23 @@
 package br.com.maiconcardoso.agendaapi;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import br.com.maiconcardoso.agendaapi.models.ContactModel;
-import br.com.maiconcardoso.agendaapi.repositories.ContactRepository;
 import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class AgendaApiApplication {
 
-	private final ContactRepository contatoRepository;
+	// private final ContactRepository contatoRepository;
 
-	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-			ContactModel contato = new ContactModel(null, "Maria", "maria@gmail.com", true);
-			contatoRepository.save(contato);
-		};
-	}
+	// @Bean
+	// public CommandLineRunner runner() {
+	// 	return args -> {
+	// 		ContactModel contato = new ContactModel(null, "Maria", "maria@gmail.com", true);
+	// 		contatoRepository.save(contato);
+	// 	};
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApiApplication.class, args);
