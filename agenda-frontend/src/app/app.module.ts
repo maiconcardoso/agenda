5b7e68128842fe8ff6edbb1d3e36d103120d5fa7
maicon_cardoso <maiconscardoso@hotmail.com>
2022-10-20 07:20:20 -0300

    Criando a tela de adicionar contatos

--- agenda-frontend/src/app/app.module.ts
+++ agenda-frontend/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactComponent } from './contact/contact.component';
+import { ContactService } from './contact.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    ContactService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
